cf0af2b797a171f1f845011b947081fbc1b33c78
Zach Weinstein <zweinstein@gmail.com>
2016-03-09 22:05:58 -0700

    submit form done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
 class UsersController < ApplicationController
 
 	def new
+		@user = User.new
 	end
 
 	def create
+		@user = User.new(username: params[:username], email: params[:email],
+			password: params[:password])
+		#@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "User saved!"
+			redirect_to @user
+		else
+			render 'new'
+		end
 	end
+
+
+	private
+
+		def user_params
+			params.require(:user).permit(:username,:email,:password)
+		end
 end
+
+
