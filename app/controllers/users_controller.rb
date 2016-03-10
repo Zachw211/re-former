0ec82314fc150c8f7bbe08a5a032529b2199383d
Zach Weinstein <zweinstein@gmail.com>
2016-03-09 22:18:29 -0700

    added edit users feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,26 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if 
+			@user.update(user_params)
+			redirect_to edit_user_path(@user)
+		else
+			render :edit
+		end
+	end
+			
+		
+
 	def create
-		@user = User.new(username: params[:username], email: params[:email],
-			password: params[:password])
-		#@user = User.new(user_params)
+		#@user = User.new(username: params[:username], email: params[:email],
+		#	password: params[:password])
+		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "User saved!"
 			redirect_to @user
@@ -22,6 +38,7 @@ class UsersController < ApplicationController
 		def user_params
 			params.require(:user).permit(:username,:email,:password)
 		end
+
 end
 
 
